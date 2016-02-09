d458e1791c921ed42d1b0fc92c9de10ac27cb132
queicherius <queicherius@gmail.com>
2016-02-09 21:43:58 +0100

    Set cache headers

--- tests/controllers/gem.spec.js
+++ tests/controllers/gem.spec.js
@@ -14,11 +14,12 @@ describe('controllers > GemController', () => {
 
   it('returns the cache object when handling requests', async () => {
     let content = {gold: [1, 2, 3], gems: [4, 5, 6]}
-    let response = {send: sinon.spy()}
+    let response = {send: sinon.spy(), cache: sinon.spy()}
     let next = sinon.spy()
     cache.gemPriceHistory = content
 
     worker.handle(null, response, next)
+    expect(response.cache.calledOnce).to.equal(true)
     expect(response.send.calledOnce).to.equal(true)
     expect(next.calledOnce).to.equal(true)
     expect(response.send.args[0][0]).to.deep.equal(content)
