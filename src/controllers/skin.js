451104c55cec60cddcbccbfb6d94f1cc473a5bc2
queicherius <queicherius@gmail.com>
2016-03-17 16:01:37 +0100

    Update skin worker and controller to use mongo

--- src/controllers/skin.js
+++ src/controllers/skin.js
@@ -1,7 +1,8 @@
-const storage = require('../helpers/sharedStorage.js')
+const mongo = require('../helpers/mongo.js')
 
-function resolve (request, response) {
-  response.send(storage.get('skinsToItems'))
+async function resolve (request, response) {
+  let skinCache = await mongo.collection('cache').find({id: 'skinsToItems'}).limit(1).next()
+  response.send(skinCache.content)
 }
 
 module.exports = {resolve}
