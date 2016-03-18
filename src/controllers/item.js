ef79a8ee2709f2db35d5cb30c6874d9b5f56dc76
queicherius <queicherius@gmail.com>
2016-03-18 12:47:41 +0100

    Filter tradable items with no prices (if that even happens for any reason)

--- src/controllers/item.js
+++ src/controllers/item.js
@@ -38,7 +38,8 @@ async function all (request, response) {
 async function allPrices (request, response) {
   let items = await mongo.collection('items').aggregate([
     {'$match': {tradable: true, lang: 'en'}},
-    {'$project': {_id: 0, id: 1, price: {'$max': ['$sell.price', '$buy.price', '$vendor_price']}}}
+    {'$project': {_id: 0, id: 1, price: {'$max': ['$sell.price', '$buy.price', '$vendor_price']}}},
+    {'$match': {price: {'$ne': null}}}
   ]).toArray()
 
   response.send(items)
