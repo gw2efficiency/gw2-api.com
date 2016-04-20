029f83cc8306755408d59ba878c6c3b6fa952bfe
queicherius <queicherius@gmail.com>
2016-04-20 16:01:07 +0200

    Add a blacklist of items that are in the tradingpost, even tho they are untradable

--- src/static/tradingpostBlacklist.js
+++ src/static/tradingpostBlacklist.js
@@ -1,10 +1,295 @@
+// These are all the items that are in the tradingpost, even tho they are not actually tradable.
+// We will throw them out, because all they do is generate confusion and distort account values.
+// Items with "precaution" added are not currently in the tradingpost, but might at some point
+// get bugged alongside the other items in their category (better safe than sorry).
+
 module.exports = [
-  // pvp reward boxes
+  // Old pvp rewards
+  21319, // Deer Reward Chest
+  21318, // Rabbit Reward Chest
+  39751, // Match Win Reward Chest
+  43936, // PvP Achievement Rewards
+  20839, // Arcane Orb
+  20800, // Arcane Crystal
+  20802, // Arcane Sliver
+  20801, // Arcane Powder
+  23046, // PvP Salvage kit
 
-  // pvp crafting materials
-
-  // pvp salvage kits
-
-  // guild decorations
-
+  // Guild decorations
+  73485, // Crooked Mushroom
+  74241, // Bonfire
+  72922, // Lion Statue
+  75375, // Creepy Jack-o'-Lantern
+  76336, // Thin Candlestick
+  71061, // Summit Banner
+  71716, // Throne
+  70777, // Potted Blooming Moa Fern
+  72053, // Loaded Wagon
+  76003, // Cheery Balloon Bundle [precaution]
+  73125, // Sylvari Summit Banner
+  70856, // Campfire
+  74970, // Potted Slender Fern Tree [precaution]
+  73594, // Hedge
+  71757, // Broken Ascalonian Pillar [precaution]
+  72172, // Lattice Planter with Loosestrife [precaution]
+  71964, // Head Topiary
+  75518, // Distressed Lion Statue
+  73946, // Mists Griffon Statue
+  73361, // Plush Armchair
+  72739, // Guild Initiative Banner
+  76971, // Fancy Table
+  75786, // Festival Tent
+  70640, // Blue Cushion [precaution]
+  72400, // Green Cushion [precaution]
+  74693, // Massive Balloon Bouquet
+  75777, // Gilded Lamp
+  71893, // Potted Shaggy Palm [precaution]
+  77230, // Spooky Cauldron
+  76231, // Mists Minotaur Statue
+  76910, // Bowl Topiary
+  75854, // Lattice Planter with Red Petunias [precaution]
+  73470, // Potted Jungle Grass
+  71077, // Basic Table [precaution]
+  74759, // Potted Sprouting Night Thistle
+  73098, // Basic Shrub [precaution]
+  74640, // Empty Square Planter
+  71986, // Human Summit Banner
+  75904, // Lattice Planter with Daisies
+  72561, // Red Flag
+  74394, // Rectangular Planter
+  72687, // Deluxe Wyvern Trophy [precaution]
+  75396, // Basic Planter [precaution]
+  71438, // Basic Bookshelf [precaution]
+  76064, // Blue Pirate Flag
+  76251, // Worn Arch
+  72488, // Gold Pillar
+  74800, // Lattice Planter with Blue Petunias
+  74487, // Simple Table
+  72780, // Wide Library Shelf
+  73455, // Potted Blue Moa Fern
+  76194, // Mists Rock Dog Statue
+  75486, // Large Festival Tent
+  74223, // Charr Statue
+  70733, // Gold Wall
+  71899, // Lattice Planter
+  74914, // White Flag
+  75108, // Potted Night Thistle Bud
+  71091, // Fine Armor Stand
+  73283, // Lion Fountain
+  74669, // Grandfather Clock
+  73390, // Broken Mast Pole
+  70459, // Potted Mature Night Thistle
+  74198, // Block Topiary
+  74066, // Guild Barstool
+  75198, // Yellow Cushion [precaution]
+  76299, // Festive Balloon Bundle
+  73166, // Capped Gold Pillar
+  72037, // Jack-o'-Lantern
+  74670, // Potted Broad Paddlefrond
+  74135, // Square Guild Bar
+  71621, // Norn Summit Flag
+  76707, // Red Cushion
+  76622, // Fancy Round Table [precaution]
+  75490, // Guild Chair
+  71320, // Lit Wagon
+  76052, // Tall Lattice
+  72457, // Hedge Pillar
+  70627, // Square Planter
+  73966, // Lattice [precaution]
+  74652, // Festive Arch
+  73528, // Griffon Statue
+  76195, // Guild Stool [precaution]
+  74490, // Rustic Brazier [precaution]
+  75179, // Tall Candlestick
+  76503, // Potted Night Thistle [precaution]
+  73118, // Deluxe Charr Copter [precaution]
+  76161, // Fun Balloon Bundle
+  74817, // Potted Djinn's Tongue
+  73947, // Ascalonian Tree
+  75222, // Gramophone
+  71599, // Uzolan's Mechanical Orchestra [precaution]
+  73934, // Human Summit Flag
+  74534, // Grenth Statue
+  76255, // Lyssa Statue
+  74615, // Basic Flagpole [precaution]
+  75902, // Mists Dolyak Statue
+  73210, // Balthazar Statue
+  74224, // Ornate Armor Stand
+  73036, // Sandstone Pillar
+  72566, // Hedge Planter
+  70526, // Green Tree
+  74834, // Fuschia Balloon
+  70591, // Griffon Fountain
+  71681, // Basic Fountain [precaution]
+  75705, // Armor Stand
+  72169, // Mossy Pillar [precaution]
+  72945, // Keg Rack
+  73856, // Asuran Summit Banner
+  76177, // Guild Bar
+  71631, // Ascalonian Lamp
+  74711, // Red Balloon
+  74248, // Fire Circle
+  73093, // White Balloon [precaution]
+  71021, // Palm Tree
+  76685, // Squat Thorny Mushroom
+  74229, // Melandru Statue
+  71777, // Potted Tree
+  74038, // Crooked Thorny Mushroom
+  72840, // Mists Drake Statue
+  76345, // Basic Chair [precaution]
+  72408, // Purple Balloon
+  70660, // Potted Cypress
+  71002, // Charr Summit Flag
+  72473, // Plush Sofa
+  75453, // Green Balloon
+  76881, // Potted Petticoat Palm [precaution]
+  70689, // Basic Candle [precaution]
+  74885, // Guild Bench
+  75298, // Highback Chair
+  71756, // Charr Summit Banner
+  73831, // Refined Street Lamp
+  76151, // Well [precaution]
+  77066, // Potted Maguuma Lily (Double Bloom)
+  73420, // Potted Gold Fern
+  70936, // Keg
+  74063, // Potted Shrub
+  76929, // Fancy Wagon
+  76163, // Potted Fruiting Night Thistle
+  74389, // Short Guild Bar
+  73255, // Ceramic Planter
+  76465, // Potted Blue Orchid
+  74104, // Sphere Topiary
+  73336, // Potted Croton
+  70843, // Square Firepit
+  71210, // Red Throw Pillow
+  71684, // Summit Flag
+  73575, // Potted Palm
+  71758, // Marriner Statue
+  75236, // Potted Fern Tree
+  71230, // Elegant Square Pillar
+  74200, // Elegant Wall Panel
+  75675, // Blue Balloon
+  72838, // Elegant Pillar
+  71062, // Yellow Balloon
+  74968, // Square Candlestick [precaution]
+  75793, // Wagon [precaution]
+  73282, // Basic Crate [precaution]
+  75823, // Deluxe Vine Wall [precaution]
+  72848, // Large Square Pillar
+  72653, // Potted Fan Palm
+  76314, // Body Topiary
+  73729, // Potted Tall Cypress
+  74876, // Simple Shelf
+  75058, // Library Shelf
+  71878, // Row of Candles
+  74354, // Potted Junglerice
+  70771, // Dwayna Statue
+  74841, // Thorny Jack-o'-Lantern
+  74563, // Pillar Candle
+  72350, // Divinity Street Lamp
+  76996, // Asuran Summit Flag
+  73994, // Illuminated Fountain [precaution]
+  71662, // Basic Pedestal [precaution]
+  73134, // Vined Lattice
+  74821, // Potted Maguuma Lily (Triple Bloom)
+  75655, // Potted Lady Palm [precaution]
+  76535, // Short Guild Banquet Table
+  71612, // Kormir Statue
+  72271, // Pumpkin [precaution]
+  77268, // Orange Balloon
+  74656, // Square Cabinet
+  72460, // Green Pirate Flag
+  73707, // Worn Pillar
+  72409, // Hedge Corner
+  73657, // Basic Column [precaution]
+  74085, // Potted Reaching Gold Fern
+  73222, // Ascalonian Pillar
+  76416, // Thorny Mushroom [precaution]
+  76304, // Tyrian Globe
+  75685, // Potted Reaching Blue Fern
+  74705, // Basic Torch [precaution]
+  72530, // Potted Bamboo Cluster
+  12147, // Mushroom
+  71278, // Basic Boulder [precaution]
+  72758, // Potted Paddlefrond
+  72198, // Immense Lion Statue
+  75451, // Gold Firepit
+  74829, // Fancy Chair
+  72103, // Basic Basket [precaution]
+  73894, // Square Guild Banquet Table
+  75167, // Woodfire Grill
+  74588, // Lattice Arbor
+  75903, // Broken Square Pillar
+  76773, // Potted Bamboo
+  73783, // Guild Banquet Table
+  71554, // Sylvari Summit Flag
+  73207, // Potted Maguuma Lily
+  74217, // Spire Topiary
+  71255, // Basic Tree [precaution]
+  70549, // Divinity Lamp
+  71828, // Lattice Planter with Orange Petunias [precaution]
+  73907, // Pew
+  72811, // Bundle of Corn Stalks
+  72039, // Norn Summit Banner
+  74398, // Elaborate Sandstone Pillar
+  77107, // Fancy Armchair
+  72026, // Long Fancy Table
+  77041, // Scarecrow
+  77480, // Commemorative Weapon Stand [precaution]
+  77629, // Snow Pile [precaution]
+  77596, // Wintersday Tree
+  77636, // Holiday Wreath
+  77573, // Festive Streetlamp
+  77640, // Reindeer Ice Sculpture
+  77613, // Snow Maker
+  77729, // Gorseval Tentacle [precaution]
+  77783, // Bronze Slothasor Trophy [precaution]
+  77721, // Silver Tequatl Trophy [precaution]
+  77710, // Silver Slothasor Trophy [precaution]
+  77706, // Slothasor Mushroom [precaution]
+  77777, // Vale Guardian Pieces [precaution]
+  77773, // Chak Gerent Eye [precaution]
+  77723, // Sabetha Flamethrower Fragment [precaution]
+  77770, // Gold Vale Guardian Trophy [precaution]
+  77753, // Bronze Sabetha Trophy [precaution]
+  77693, // Red Festival Umbrella
+  77734, // Tequatl Tailbone [precaution]
+  68614, // Red Lantern [precaution]
+  77700, // Monkey Statue
+  77759, // Shatterer Crystal [precaution]
+  77789, // Bronze Triple Trouble Trophy [precaution]
+  77679, // White Mantle Abomination Crystal [precaution]
+  77697, // Bronze Chak Gerent Trophy [precaution]
+  77703, // Triple Trouble Tooth [precaution]
+  77787, // Gold Sabetha Trophy [precaution]
+  77733, // Silver Vale Guardian Trophy [precaution]
+  77715, // Silver Mordremoth Trophy [precaution]
+  77707, // Firecracker
+  77749, // Bronze Shatterer Trophy [precaution]
+  77764, // Silver White Mantle Abomination Trophy [precaution]
+  77726, // Lunar Arch
+  77730, // Red Festival Tent
+  77741, // Bronze Gorseval Trophy [precaution]
+  77702, // Silver Shatterer Trophy [precaution]
+  77782, // Silver Sabetha Trophy [precaution]
+  77732, // Bronze Tequatl Trophy [precaution]
+  77752, // Bronze White Mantle Abomination Trophy [precaution]
+  77757, // Bronze Mordremoth Trophy [precaution]
+  77725, // Ram Statue
+  77682, // Gold Gorseval Trophy [precaution]
+  77746, // Silver Gorseval Trophy [precaution]
+  77788, // Silver Chak Gerent Trophy [precaution]
+  77683, // Mordremoth Mandible [precaution]
+  78076, // Super Pagoda Column
+  78041, // Super Tree
+  78027, // Super Cloud [precaution]
+  78094, // Painting of Moto
+  78085, // Super Rock Platform
+  78108, // Super Rock Wall
+  78034, // Super Mountain
+  78074, // Unimpressive King Frog
+  78065, // Super Rock
+  78043, // Super Small Rock
+  78084, // Super King Frog
+  78095 // Super Rock Ramp
 ]
