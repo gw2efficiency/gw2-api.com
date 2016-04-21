5f0e37f18c4d4289f4f018549735943ae6fdc0cb
queicherius <queicherius@gmail.com>
2016-04-21 11:25:14 +0200

    Revert "Remove merchant custom recipes"
    This reverts commit b55fc416fb44370561a8d31c7c655b6f9b55afb2.

--- tests/workers/recipe.spec.js
+++ tests/workers/recipe.spec.js
@@ -184,18 +184,17 @@ describe('workers > recipe worker', () => {
 
   it('loads and filters custom recipes correctly', async () => {
     let input = [
-      {output_item_id: -1, ingredients: [{item_id: 5}], disciplines: ['Mystic Forge']},
-      {output_item_id: 2, ingredients: [{item_id: -5}], disciplines: ['Mystic Forge']},
-      {output_item_id: 3, ingredients: [{item_id: 5}], disciplines: ['Mystic Forge']},
-      {output_item_id: 4, ingredients: [{item_id: 4}], disciplines: ['Mystic Forge']},
-      {output_item_id: 38023, ingredients: [{item_id: 38024}], disciplines: ['Mystic Forge']},
-      {output_item_id: 38116, ingredients: [{item_id: 6}], disciplines: ['Mystic Forge']},
-      {output_item_id: 5, ingredients: [{item_id: 6}], disciplines: ['Achievement']},
-      {output_item_id: 6, ingredients: [{item_id: 99}], disciplines: ['Merchant']}
+      {output_item_id: -1, ingredients: [{item_id: 5}]},
+      {output_item_id: 2, ingredients: [{item_id: -5}]},
+      {output_item_id: 3, ingredients: [{item_id: 5}]},
+      {output_item_id: 4, ingredients: [{item_id: 4}]},
+      {output_item_id: 38023, ingredients: [{item_id: 38024}]},
+      {output_item_id: 38116, ingredients: [{item_id: 6}]},
+      {output_item_id: 5, ingredients: [{item_id: 6}]}
     ]
     let output = [
-      {output_item_id: 3, ingredients: [{item_id: 5}], disciplines: ['Mystic Forge']},
-      {output_item_id: 5, ingredients: [{item_id: 6}], disciplines: ['Achievement']}
+      {output_item_id: 3, ingredients: [{item_id: 5}]},
+      {output_item_id: 5, ingredients: [{item_id: 6}]}
     ]
 
     requesterMock.addResponse(input)
